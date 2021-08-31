b54361093b32c76d832d1513ec83f72c5a916d9e
Silvina Lorena <silvyhs.campanitah@hotmail.com>
2021-08-31 11:39:36 -0300

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,10 @@ class MyDocument extends Document {
         <Head>
             <title>Mi Portfolio</title>
             <meta name="description" content= "Silvina Saucedo Portfolio Website" />
+            <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+            <link rel="icon" href="/favicon.ico" type="image/x-icon" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"/>
+          
             </Head>
         <body>
           <Main />
